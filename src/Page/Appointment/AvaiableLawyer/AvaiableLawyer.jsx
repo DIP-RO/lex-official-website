423d3a8e88ec6235d44bda4eb3921b4054c7b21a
Dipro <dipropaul032@gamil.com>
2023-10-28 21:01:24 +0600

    Your commit message here

--- src/Page/Appointment/AvaiableLawyer/AvaiableLawyer.jsx
+++ src/Page/Appointment/AvaiableLawyer/AvaiableLawyer.jsx
@@ -23,25 +23,29 @@ const AvaiableLawyer = () => {
     ]
 
     return (
-        <div className="grid lg:mb-10 md:mb-10 lg:grid-cols-3 md:grid-cols-2 grid-cols-2 lg:gap-14 md:gap-14 mt-10 sm:max-w-sm sm:gap-4 ">
-            {testymony.map((testy) => (
-                <Link key={testy.id} to="/lawyardetails">
-                    <button className="flex btn flex-col gap-5 lg:w-[270px] lg:h-[350px] md:w-[270px] md:h-[350px] sm:h-48 sm:w-44 items-center p-8 rounded-[22px] bg-[#1d344a] hover:bg-[#1d344a]">
-                        <div className="flex flex-col justify-center items-center">
-                            <div>
-                                <img src={testy.image} className="w-[75px] h-[75px] rounded-full online" />
-                            </div>
+        <div className="h-[450px] grid lg:grid-cols-3 md:grid-cols-2 grid-cols-2 lg:gap-14 md:gap-14 mt-10 sm:max-w-sm sm:gap-4 ">
+                    {testymony.map((testy) => (
+                        <Link  key={testy.id} to="/lawyardetails">
+                        <button
+                           
+                
+                            className="flex btn flex-col gap-5 lg:w-[270px] lg:h-[350px] md:w-[270px] md:h-[350px] sm:h-48 sm:w-44 items-center p-8 rounded-[22px] bg-[#1d344a] hover:bg-[#1d344a]"
+                        >
+                           <div className="flex flex-col justify-center items-center ">
+                           <div>
+                           <img src={testy.image} className="w-[75px] h-[75px] rounded-full online" />
+                           </div>
                             <div className="text-center font-['Montserrat'] font-bold tracking-[2] leading-[19.2px] uppercase text-[#bdcdf1] mb-0">
                                 {testy.name}
                             </div>
-                            <div className="text-center font-['Open_Sans'] leading-[25.6px] text-white md:hidden">
+                            <div className="text-center font-['Open_Sans'] leading-[25.6px] text-white sm:hidden">
                                 {testy.message}
                             </div>
-                        </div>
-                    </button>
-                </Link>
-            ))}
-        </div>
+                           </div>
+                        </button>
+                        </Link>
+                    ))}
+                </div>
     );
 };
 
